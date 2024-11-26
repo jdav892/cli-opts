d3c1f3fc61415144489ff44436cea0e1f576f042
jdav892 <jadavila9@gmail.com>
2024-11-26 08:37:06 -0500

    feat: using printf function and an rgb variable to change the color of the string

--- bw.go
+++ bw.go
@@ -13,5 +13,10 @@ func main() {
 	for i := 1; i < 3; i++ {
 		phrases = append(phrases, faker.Hacker().Phrases()...)
 	}
-	fmt.Println(strings.Join(phrases[:], "; "))
+	output := strings.Join(phrases[:], "; ")
+	r, g, b := 255, 50, 0 //gold
+
+	for j := 0; j < len(output); j++ {
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
 }
